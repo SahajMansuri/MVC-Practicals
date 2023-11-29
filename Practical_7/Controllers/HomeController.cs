38672627a6f5ac6abf8e77e6db1bfc1cf9a755a9
SahajMansuri <mansurisahaj@gmail.com>
2023-11-29 14:53:44 +0530

    add practical 7 Registration form with validate the page by using data annotation concept

---
+++ Practical_7/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using Practical_7.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Practical_7.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(Student obj)
+        {
+            if (ModelState.IsValid)
+            {
+                return View("dataAdd");
+            }
+            return View();
+        }
+        public ActionResult dataAdd()
+        {
+            return View();
+        }
+    }
+}
---
+++ Practical_7/Models/Student.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Practical_7.Models
+{
+    public class Student
+    {
+       
+        [Required]
+        public string name { get; set; }
+        [Required]
+        [EmailAddress]
+        public string email { get; set; }
+        [Required]
+        [Range(18,100)]
+        public int age { get; set; }
+        [Required]
+        public string adderss { get; set; }
+    }
+}
